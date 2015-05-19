29be0621311874f24d0f06fcd50893adb26ebaa5
sairre <jsairre@163.com>
2015-05-19 12:04:33 +0800

    go on 1

--- ERROR CORRECTION/ThreadPool/ThreadPool.cpp
+++ ERROR CORRECTION/ThreadPool/ThreadPool.cpp
@@ -20,6 +20,70 @@ ThreadPool::ThreadPool(std::size_t bufSize,std::size_t threadsNum):
 void ThreadPool::start()
 {
 	std::vector<Thread *>::iterator it=threadsVec_.begin();
+	for(;it!=threadsVec_.end();++it)
+	{
+		(*it)->start();
+	}
+	isStarting_=true;
 }
+void ThreadPool::stop()
+{
+	if(isStarting_)
+	{
+		while(!buffer_.empty())
+			buffer_.deleteElement();
+		std::vector<Thread *>::iterator it=threadsVec_.begin();
+		for(;it!=threadsVec_.end();++It)
+		{
+			(*it)->join();
+		}
+		threadsVec_=clear();
+	}
+	isStarting_=false;
+}
+void ThreadPool::addTask(Task * pTask)
+{
+	buffer_.addElement(pTask);
+}
+
+void ThreadPool::getTask()
+{
+	Task *pTask=buffer_.deleteElement();
+	return pTask;
+}
+void ThreadPool::runInThread()
+{
+	while(1)
+	{
+		Task *pTask=getTask();
+		pTask->process();
+	}
+}
+ThreadPool::~ThreadPool()
+{
+	stop();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
