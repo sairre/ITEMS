378fdffc68f7cf7346229b206e58d0522c146a70
sairre <jsairre@163.com>
2015-05-21 16:41:49 +0800

    8.5

---
+++ C++ Primer Exercise/CH8/8.5/8.5.cpp
@@ -0,0 +1,34 @@
+/*************************************************************************
+	> File Name: 8.4.cpp
+	> Author: SAIRRE
+	> Mail:496389633@qq.com 
+	> Created Time: Thu 21 May 2015 04:08:32 PM CST
+ ************************************************************************/
+
+#include<iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+int main(void)
+{
+	std::ifstream in("data");
+	if(!in)
+	{
+		std::cerr<<"open in fail"<<std::endl;
+		return -1;
+	}
+	std::string line;
+	std::vector<std::string> words;
+//	while(getline(in,line))
+	while(in>>line)
+	{
+		words.push_back(line);
+	}
+	in.close();
+	std::vector<std::string>::const_iterator it=words.begin();
+	while(it!=words.end())
+	{
+		std::cout<<*it<<std::endl;
+		++it;
+	}
+}
