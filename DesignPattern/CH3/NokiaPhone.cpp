2a3853a5f1f33f3abfcf9e3521b91a45dcc530cd
sairre <jsairre@163.com>
2015-06-15 22:38:41 +0800

    go on

--- DesignPattern/CH3/NokiaPhone.cpp
+++ DesignPattern/CH3/NokiaPhone.cpp
@@ -4,8 +4,19 @@
 	> Mail:496389633@qq.com 
 	> Created Time: Sat 13 Jun 2015 11:32:23 AM CST
  ************************************************************************/
-
+#include "NokiaPhone.h"
 namespace Ch
 {
-
+NokiaPhone::NokiaPhone()
+{
+	std::cout<<"default NokiaPhone constructor"<<std::endl;
+}
+NokiaPhone::~NokiaPhone()
+{
+	std::cout<<"default NokiaPhone destroy"<<std::endl;
+}
+void NokiaPhone::ShowDecoreate()
+{
+	std::cout<<"NokiaPhone"<<m_name<<" ShowDecoreate "<<std::endl;
+}
 }
--- DesignPattern/CH3/iPhone.h
+++ DesignPattern/CH3/iPhone.h
@@ -1,26 +1,24 @@
 /*************************************************************************
-	> File Name: NokiaPhone.h
+	> File Name: iPhone.h
 	> Author: SAIRRE
 	> Mail:496389633@qq.com 
-	> Created Time: Sat 13 Jun 2015 11:11:25 AM CST
+	> Created Time: Mon 15 Jun 2015 10:34:38 PM CST
  ************************************************************************/
-#ifndef CH_PHONE_H
-#define CH_PHONE_H
-#include <string>
-#include <iostream>
+
+#ifndef CH_IPHONE_H
+#define CH_IPHONE_H
 #include "Phone.h"
 namespace Ch
 {
-class NokiaPhone:Public Phone
+class iPhone:public Phone
 {
 public:	
-	NokiaPhone();
-	NokiaPhone(std::string str):m_name(str)
+	iPhone();
+	iPhone(std::string str):m_name(str)
 	{}
-
-	~NokiaPhone();
+	~iPhone();
 	void ShowDecoreate();
 private:
 	std::string m_name;
-};
+}
 }
